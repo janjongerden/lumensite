c33519592280dc534f01ebe38676b4dbc69381e8
jan <jan@example.org>
2016-02-04 23:29:37 +0100

    use full url for json

--- angular.js
+++ angular.js
@@ -1,8 +1,8 @@
 var app = angular.module('app', []);
 
 app.controller('AppCtrl', function ($scope, $http) {
-//	$http.get('http://janjongerden.nl/lumen/movies.json')
-	$http.get('movies.json')
+	$http.get('http://janjongerden.nl/lumen/movies.json')
+//	$http.get('movies.json')
 		   .then(function(res){
 			  $scope.movies = res.data;                
 			});
