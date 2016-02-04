f2d51e72407afe577e92840d7dabc6cd76539fd2
jan <jan@example.org>
2016-02-04 23:24:43 +0100

    inital commit

---
+++ angular.js
@@ -0,0 +1,10 @@
+var app = angular.module('app', []);
+
+app.controller('AppCtrl', function ($scope, $http) {
+//	$http.get('http://janjongerden.nl/lumen/movies.json')
+	$http.get('movies.json')
+		   .then(function(res){
+			  $scope.movies = res.data;                
+			});
+ }
+);
