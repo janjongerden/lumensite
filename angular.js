c1a6faf7ea11c13b67889837bb739c915acfa772
jan <jan@example.org>
2016-02-15 22:59:33 +0100

    ng uses the file screenings instead of movies.json

--- angular.js
+++ angular.js
@@ -1,10 +1,10 @@
 var app = angular.module('app', []);
 
 app.controller('AppCtrl', function ($scope, $http) {
-	$http.get('http://janjongerden.nl/lumen/movies.json')
+	$http.get('screenings.json')
 //	$http.get('movies.json')
 		   .then(function(res){
-			  $scope.movies = res.data;                
+			  $scope.screenings = res.data;                
 			});
  }
 );
